f39e3859b0ce38509110afbed561ea578759d47b
Brendan C <95544071+BrendanClive@users.noreply.github.com>
2023-02-04 16:14:42 -0500

    (in the process of) switching Xbox to Joystick

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
@@ -42,7 +43,7 @@ public class RobotContainer {
   private final ArmSubsystem m_robotArm = new ArmSubsystem();
 
   // The driver's controller
-  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+  Joystick m_driverJoystick = new Joystick(OIConstants.kDriverJoystickPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -62,11 +63,11 @@ public class RobotContainer {
             () ->
                 m_robotDrive.drive(
                     -MathUtil.applyDeadband(
-                        m_driverController.getLeftY(), OIConstants.kDriveDeadband),
+                        m_driverJoystick.getLeftY(), OIConstants.kDriveDeadband),
                     -MathUtil.applyDeadband(
-                        m_driverController.getLeftX(), OIConstants.kDriveDeadband),
+                        m_driverJoystick.getLeftX(), OIConstants.kDriveDeadband),
                     -MathUtil.applyDeadband(
-                        m_driverController.getRightX(), OIConstants.kDriveDeadband),
+                        m_driverJoystick.getRightX(), OIConstants.kDriveDeadband),
                     true,
                     true),
             m_robotDrive));
@@ -83,18 +84,18 @@ public class RobotContainer {
    * {@link JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(m_driverController, Button.kR1.value)
+    new JoystickButton(m_driverJoystick, Button.kR1.value)
         .whileTrue(new RunCommand(() -> m_robotDrive.setX(), m_robotDrive));
 
     // The buttons created below were meant for LED testing, feel free to change
 
-    new JoystickButton(m_driverController, Button.kCircle.value)
+    new JoystickButton(m_driverJoystick, Button.kCircle.value)
         .whileTrue(new RunCommand(() -> m_LEDSubsystem.WarningLight(), m_LEDSubsystem));
 
-    new JoystickButton(m_driverController, Button.kCross.value)
+    new JoystickButton(m_driverJoystick, Button.kCross.value)
         .whileTrue(new RunCommand(() -> m_LEDSubsystem.ReadyLight(), m_LEDSubsystem));
 
-    new JoystickButton(m_driverController, Button.kSquare.value)
+    new JoystickButton(m_driverJoystick, Button.kSquare.value)
         .whileTrue(new RunCommand(() -> m_LEDSubsystem.LightOff(), m_LEDSubsystem));
   }
 
