0244636048d18aa82eb08d34f63f18585a72f5ad
Brendan C <95544071+BrendanClive@users.noreply.github.com>
2023-02-16 20:03:26 -0500

    Adding PID to GravityOffset in ArmSubsystem

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -8,6 +8,7 @@ import com.revrobotics.SparkMaxPIDController;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 
@@ -17,9 +18,28 @@ public class ArmSubsystem extends SubsystemBase implements AutoCloseable {
   private final AbsoluteEncoder ArmEncoder;
   public static DoubleSolenoid ExtendingSolenoid;
   public static DoubleSolenoid ClawSolenoid;
-  private final SparkMaxPIDController ArmPIDController;
+  private SparkMaxPIDController ArmPIDController;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
 
   public ArmSubsystem() {
+    kP = 0.1;
+    kI = 1e-4;
+    kD = 1;
+    kIz = 0;
+    kFF = 0.7;
+    kMaxOutput = 1;
+    kMinOutput = -1;
+
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
+    SmartDashboard.putNumber("Set Rotations", 0);
+
     MasterArmMotor = new CANSparkMax(ArmConstants.kMasterArmMotorPort, MotorType.kBrushless);
     MinionArmMotor = new CANSparkMax(ArmConstants.kMinionArmMotorPort, MotorType.kBrushless);
 
@@ -40,6 +60,13 @@ public class ArmSubsystem extends SubsystemBase implements AutoCloseable {
 
     ArmPIDController = MasterArmMotor.getPIDController();
     ArmPIDController.setFeedbackDevice(ArmEncoder);
+
+    ArmPIDController.setP(kP);
+    ArmPIDController.setI(kI);
+    ArmPIDController.setD(kD);
+    ArmPIDController.setIZone(kIz);
+    ArmPIDController.setFF(kFF);
+    ArmPIDController.setOutputRange(kMinOutput, kMaxOutput);
   }
 
   public void GravityOffset(int ktargetPos) {
@@ -101,6 +128,46 @@ public class ArmSubsystem extends SubsystemBase implements AutoCloseable {
   @Override
   public void periodic() {
     ArmEncoder.getPosition();
+    double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+    double rotations = SmartDashboard.getNumber("Set Rotations", 0);
+
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
+    if ((p != kP)) {
+      ArmPIDController.setP(p);
+      kP = p;
+    }
+    if ((i != kI)) {
+      ArmPIDController.setI(i);
+      kI = i;
+    }
+    if ((d != kD)) {
+      ArmPIDController.setD(d);
+      kD = d;
+    }
+    if ((iz != kIz)) {
+      ArmPIDController.setIZone(iz);
+      kIz = iz;
+    }
+    if ((ff != kFF)) {
+      ArmPIDController.setFF(ff);
+      kFF = ff;
+    }
+    if ((max != kMaxOutput) || (min != kMinOutput)) {
+      ArmPIDController.setOutputRange(min, max);
+      kMinOutput = min;
+      kMaxOutput = max;
+    }
+
+    ArmPIDController.setReference(rotations, CANSparkMax.ControlType.kPosition);
+
+    SmartDashboard.putNumber("SetPoint", rotations);
+    SmartDashboard.putNumber("ProcessVariable", ArmEncoder.getPosition());
   }
 
   @Override
