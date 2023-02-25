32b16c7d7ca2e4854c79a3dadd4ee082e91c49f8
Alex Swindell <alxswindell@gmail.com>
2023-02-25 16:13:32 -0500

    Just moved the trajectory to a command

--- src/main/java/frc/robot/commands/AutoDriveToGridPos1Command.java
+++ src/main/java/frc/robot/commands/AutoDriveToGridPos1Command.java
@@ -2,6 +2,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.*;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import java.util.List;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 
 public class AutoDriveToGridPos1Command extends CommandBase {
 
@@ -14,5 +27,47 @@ public class AutoDriveToGridPos1Command extends CommandBase {
     m_vision = v_subsystem;
     addRequirements(d_subsystem);
     addRequirements(v_subsystem);
+  }
+
+  @Override
+  public void execute() {
+    TrajectoryConfig config =
+    new TrajectoryConfig(
+            AutoConstants.kMaxSpeedMetersPerSecond,
+            AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+        // Add kinematics to ensure max speed is actually obeyed
+        .setKinematics(DriveConstants.kDriveKinematics);
+
+    // An example trajectory to follow. All units in meters.
+    Trajectory exampleTrajectory =
+      TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        // Pass through these two interior waypoints, making an 's' curve path
+        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+        // End 3 meters straight ahead of where we started, facing forward
+        new Pose2d(3, 0, new Rotation2d(0)),
+        config);
+
+    ProfiledPIDController thetaController =
+      new ProfiledPIDController(
+        AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+      thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+    SwerveControllerCommand swerveControllerCommand =
+      new SwerveControllerCommand(
+        exampleTrajectory,
+        m_drive::getPose, // Functional interface to feed supplier
+        DriveConstants.kDriveKinematics,
+
+        // Position controllers
+        new PIDController(AutoConstants.kPXController, 0, 0),
+        new PIDController(AutoConstants.kPYController, 0, 0),
+        thetaController,
+        m_drive::setModuleStates,
+        m_drive);
+
+    // Reset odometry to the starting pose of the trajectory.
+    m_drive.resetOdometry(exampleTrajectory.getInitialPose());
   }
 }
