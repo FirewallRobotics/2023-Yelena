da609a73a37dab649d2cdbf337ed1fa39762fe14
Brendan C <95544071+BrendanClive@users.noreply.github.com>
2023-03-31 19:12:19 -0400

    Spotless Apply

--- src/main/java/frc/robot/commands/AutoDriveToBalanceBackwardsCommand.java
+++ src/main/java/frc/robot/commands/AutoDriveToBalanceBackwardsCommand.java
@@ -15,9 +15,9 @@ public class AutoDriveToBalanceBackwardsCommand extends CommandBase {
 
   private double driveToBalanceSpeed = Constants.AutoConstants.kDriveToBalanceSpeedMultiplier;
   private double driveGyroAngleRange = Constants.AutoConstants.kDriveToBalanceGyroAngleRange;
-  //private double driveToBalanceDelay = Constants.AutoConstants.kDriveToBalanceDelay;
+  // private double driveToBalanceDelay = Constants.AutoConstants.kDriveToBalanceDelay;
   private boolean onBalance = false;
-  //private int timer = 0;
+  // private int timer = 0;
 
   public AutoDriveToBalanceBackwardsCommand(DriveSubsystem d_subsystem) {
     m_drive = d_subsystem;
